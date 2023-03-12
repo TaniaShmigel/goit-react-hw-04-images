0dde06d8d8951ab5c97bdfea2c8e430c00342b08
TaniaShmigel <tatjanka222@gmail.com>
2023-03-12 23:22:03 +0200

    Fixed Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -5,17 +5,16 @@ import { useEffect } from 'react';
 const modalRoot = document.querySelector('#modal-root');
 
 const Modal = ({ children, onShow }) => {
-  const handleKey = e => {
-    if (e.code === 'Escape') {
-      onShow();
-    }
-  };
-
   useEffect(() => {
+    const handleKey = e => {
+      if (e.code === 'Escape') {
+        onShow();
+      }
+    };
     window.addEventListener('keydown', handleKey);
 
     return () => window.removeEventListener('keydown', handleKey);
-  });
+  }, [onShow]);
 
   const handleBackdropClick = e => {
     if (e.target === e.currentTarget) onShow();
